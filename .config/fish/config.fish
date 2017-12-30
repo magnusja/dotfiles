8ebeb68bd5f7800ee462be4ad3cade2db33cf8ab
Magnus <jahnen@in.tum.de>
2017-12-29 20:52:43 -0800

    Update anaconda3 path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,7 +16,7 @@ end
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME/'
 alias init_docker='docker-machine start default; eval (docker-machine env default)'
 alias init_anaconda2='set -x PATH ~/anaconda2/bin $PATH'
-alias init_anaconda3='set -x PATH ~/Library/anaconda3/bin $PATH'
+alias init_anaconda3='set -x PATH /usr/local/anaconda3/bin $PATH'
 
 set -x EDITOR /usr/bin/vim
 
