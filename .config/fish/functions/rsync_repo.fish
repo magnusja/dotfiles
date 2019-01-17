61dba6633e1adbbbbafe0812cff6ac5b87cb913f
Magnus <jahnen@in.tum.de>
2019-01-17 11:26:54 +0100

    do not delete on remote

--- .config/fish/functions/rsync_repo.fish
+++ .config/fish/functions/rsync_repo.fish
@@ -1,3 +1,3 @@
 function rsync_repo
-	rsync -azhP --delete --exclude .git --exclude-from=$argv[1]/.gitignore $argv[1] $argv[2] 
+	rsync -azhP  --exclude .git --exclude-from=$argv[1]/.gitignore $argv[1] $argv[2] 
 end
